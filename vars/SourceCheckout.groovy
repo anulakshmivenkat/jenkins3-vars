3cb81f942c07ebe4bcd30090801fbf4fb7a48cb8
st-naresh <127725544+st-naresh@users.noreply.github.com>
2023-03-27 15:32:51 +0530

    Update SourceCheckout.groovy

--- vars/SourceCheckout.groovy
+++ vars/SourceCheckout.groovy
@@ -7,7 +7,7 @@ def call (def giturl, def branch) {
     if(giturl.contains("github.com"))
     {
         checkout([$class: 'GitSCM', branches: [[name: branch ]], extensions: [], userRemoteConfigs: [
-            [credentialsId: 'github', url: giturl]
+            [credentialsId: 'shared', url: giturl]
         ]])
     }
     else
