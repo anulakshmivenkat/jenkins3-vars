773892176c058479b1879220e8126e755c7fd982
st-naresh <127725544+st-naresh@users.noreply.github.com>
2023-03-28 12:33:17 +0530

    Update jenkinsForJava.groovy

--- vars/jenkinsForJava.groovy
+++ vars/jenkinsForJava.groovy
@@ -19,7 +19,7 @@ def call(String repoUrl) {
            }
            stage("Checkout Code") {
                steps {
-                   git branch: 'master',credentialsId: 'shared',
+                   git branch: 'master',
                        url: "${repoUrl}"
                }
            }
